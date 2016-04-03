62e38d7f019a970311ef89a68c2c38085b3a3aa6
maxuepo <maxuepo@gmail.com>
2016-04-03 12:14:02 -0700

    added problem statement

--- src/main/java/net/xuepo/algorithm/CountBits.java
+++ src/main/java/net/xuepo/algorithm/CountBits.java
@@ -2,17 +2,20 @@ package net.xuepo.algorithm;
 
 /**
  * Created by xuepo on 4/3/16.
+ *
+ * Statement:
+ * Given a non negative integer number num.
+ * For every numbers i in the range 0 ≤ i ≤ num calculate
+ * the number of 1's in their binary representation and
+ * return them as an array.
+ *
  */
 public class CountBits {
-
     public int[] countBits(int num) {
         int[] res = new int[num + 1];
-
         for (int i = 1; i <= num; i++) {
             res[i] = i % 2 + res[i/2];
         }
-
         return res;
-
     }
 }
