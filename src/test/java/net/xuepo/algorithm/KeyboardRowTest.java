dbafcac8a536fa826e8f36cca8e40df01cb7de30
maxuepo <maxuepo@gmail.com>
2017-02-07 23:22:27 -0800

    updated the unit test.

--- src/test/java/net/xuepo/algorithm/KeyboardRowTest.java
+++ src/test/java/net/xuepo/algorithm/KeyboardRowTest.java
@@ -1,5 +1,6 @@
 package net.xuepo.algorithm;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.*;
@@ -8,13 +9,12 @@ import static org.testng.Assert.*;
  * Created by xuepo on 2/7/17.
  */
 public class KeyboardRowTest extends KeyboardRow {
-
     @Test
     public void test(){
         String[] input = {"Hello", "Alaska", "Dad", "Peace"};
         String[] output = findWords(input);
-        for(String word : output) {
-            System.out.println(word);
-        }
+        Assert.assertEquals(output.length, 2);
+        Assert.assertEquals(output[0], "Alaska");
+        Assert.assertEquals(output[1], "Dad");
     }
 }
